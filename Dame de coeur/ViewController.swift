9fd77c9cc54a2089280f6b801e81f2e149b58668
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-08-21 16:28:21 +0200

    interfaces buttons

--- Dame de coeur/ViewController.swift
+++ Dame de coeur/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var carte0: UIButton!
+    @IBOutlet weak var carte1: UIButton!
+    
+    @IBOutlet weak var carte2: UIButton!
+    @IBOutlet weak var carte3: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func carteChoisie(_ sender: Any) {
     }
-
+    
 
 }
 
